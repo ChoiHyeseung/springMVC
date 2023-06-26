7ed35b05f75ff192b4b3096bcdd9088ab3599d63
Choi, Hyeseung <qorgus31@naver.com>
2023-06-26 19:24:19 +0900

    회원관리 웹 애플리케이션 리포지토리 작성

--- servlet/src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ servlet/src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -1,6 +1,8 @@
 package hello.servlet.domain.member;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 //동시성 문제가 고려되어 있지 않음. 실무에서는 ConcurrentHashMap, AtomicLong 사용고려
@@ -24,5 +26,17 @@ public class MemberRepository {
         return member;
     }
 
-    public Member
+    public Member findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Member> findAll(){
+        // arraylist를 조작해도 store의 값을 건들고 싶지 않기 때문에 별도로 새로 만들어 조회한다.
+        return new ArrayList<>(store.values());
+    }
+
+    // 테스트 코드에서만 사용
+    public void clearStore(){
+        store.clear();
+    }
 }
